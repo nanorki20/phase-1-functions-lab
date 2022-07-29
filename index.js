070c7a893f42bfcdb9f1b08bdba522624cdd10d2
Hayet Kumasseh <naaakleh@gmail.com>
2022-07-29 15:59:46 -0400

    commit tree c5c51ca64a73358b7fa2876c0f48d13f0c037b17

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickUp) {
+   const hQ = 42;
+   let blocks = Math.abs(hQ - pickUp);
+   return blocks
+};
+
+function distanceFromHqInFeet(distanceFromHqInBlocks) {
+    return Math.abs((distanceFromHqInBlocks - 42) * 264)
+}
+
+
+function distanceTravelledInFeet(pickUp, destination) {
+    return Math.abs((destination - pickUp) * 264)
+}
+
+
+function calculatesFarePrice(start, destination){
+    let feet = Math.abs((destination - start) * 264);
+     
+    if (feet < 400) {
+        return 0;
+    } else if (feet > 400 && feet <=2000) {
+        return ((feet-400) * 0.02);
+    } else if (feet <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far'
+    }
+};
